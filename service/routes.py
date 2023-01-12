9be979b8ac8ccb0f25f92e9f9f5977b99927dd06
andreobereigner <andre@obereigner.de>
2023-01-12 11:41:46 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -109,7 +109,7 @@ def update_accounts(account_id):
 
 
 ######################################################################
-# DELETE AN ACCOUNT
+# DELETE AN ACCOUNT 
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
